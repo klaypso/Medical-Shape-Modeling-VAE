f17cb52522e38a12726c9e37a6bca9208d783241
klaypso <klaypso@proton.me>
2023-07-29 07:02:46 +0000

    Updated common version in master to work around jgitflow bug

---
+++ data/data_process.py
@@ -0,0 +1,8 @@
+import numpy as np
+import os.path as path
+import nibabel as nib
+from skimage.transform import resize
+import os
+import glob
+
+image_path = '<path-to-the-image>/nih_data/Pancreas
