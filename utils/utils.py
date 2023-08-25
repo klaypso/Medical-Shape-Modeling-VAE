4cceedf110d287a4d6cc5de2245a297c4c80d1f2
klaypso <klaypso@proton.me>
2023-08-25 09:05:18 +0000

    Update something

--- utils/utils.py
+++ utils/utils.py
@@ -17,4 +17,16 @@ import imageio
 import json
 from batchgenerators.transforms.spatial_transforms import SpatialTransform, augment_spatial
 
-def Tag(temp,index,tempC,num_compon
+def Tag(temp,index,tempC,num_component):
+    tempC[index[0],index[1],index[2]]=num_component
+    expand = []
+    expand.append(index)
+    count=1
+    while len(expand)>0:
+        temp_index = expand.pop()
+        for i in range(-1,2):
+            for j in range(-1,2):
+                for k in range(-1,2):
+                    new_index = [min(max(temp_index[0]+i,0),temp.shape[0]-1),min(max(temp_index[1]+j,0),temp.shape[1]-1),min(max(temp_index[2]+k,0),temp.shape[2]-1)]
+                    if temp[new_index[0],new_index[1],new_index[2]]==1 and tempC[new_index[0],new_index[1],new_index[2]]==0:
+               
