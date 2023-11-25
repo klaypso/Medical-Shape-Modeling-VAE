f35e468af82003b9b177dd4f7d7451a2fea2930b
klaypso <klaypso@proton.me>
2023-11-25 05:48:02 +0000

    6.4.2

--- utils/utils.py
+++ utils/utils.py
@@ -831,4 +831,18 @@ def mutual_information_3d(x, y, sigma=1, normalized=True):
     sh = np.sum(jh)
     jh = jh / sh
     s1 = np.sum(jh, axis=0).reshape((-1, jh.shape[0]))
-    s2 = np.sum(
+    s2 = np.sum(jh, axis=1).reshape((jh.shape[1], -1))
+
+    # Normalised Mutual Information of:
+    # Studholme,  jhill & jhawkes (1998).
+    # "A normalized entropy measure of 3-D medical image alignment".
+    # in Proc. Medical Imaging 1998, vol. 3338, San Diego, CA, pp. 132-143.
+    if normalized:
+        mi = ((np.sum(s1 * np.log(s1)) + np.sum(s2 * np.log(s2))) / np.sum(jh * np.log(jh))) - 1
+    else:
+        mi = (np.sum(jh * np.log(jh)) - np.sum(s1 * np.log(s1)) - np.sum(s2 * np.log(s2)))
+
+    return mi
+def plot_slides(v, display_type='TB'):
+
+    # Plot the slid
